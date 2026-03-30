7bcc877c9cf361e9c88250585b8e05609121f38f
Conelius Kavoo <kavoocornelius7@gmail.com>
2026-03-30 14:46:42 +0300

    Refactor index.js to add tax calculation, string conversion, maximum finder, palindrome checker, and discounted price functions

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
 
+function convertToUpperCase(text) {
+  return String(text).toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+  return num1 >= num2 ? num1 : num2;
+}
+
+function isPalindrome(word) {
+  const str = String(word);
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice * (1 - discountPercentage / 100);
+}
 
 
 
